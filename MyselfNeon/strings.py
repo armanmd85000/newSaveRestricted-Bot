5538abdc2b8bcf4bb7d8877249f242432d91a3d8
NeonAnurag <justasneon@gmail.com>
2025-05-15 07:30:04 +0530

    Update strings.py

--- MyselfNeon/strings.py
+++ MyselfNeon/strings.py
@@ -2,14 +2,12 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-HELP_TXT = """**<u>‼️ HELP MENU ‼️</u>** 
-
-**⁉️ FOR PRIVATE CHATS** 👀
+HELP_TXT = """⁉️ **<u>FOR PRIVATE CHATS</u>** 👀
 
 **__First send invite link of the Chat then send post/s link Correctly 🖇️ \n(Unnecessary if the Account of String Session is already member of the Chat)__**
 
 
-**⁉️ FOR BOT CHATS** 🤖
+⁉️ <u>**FOR BOT CHATS**</u> 🤖
 
 **__Send link with '/b/', Bot's Username and message id, you might need to Install some Unofficial client to get the ID like below__**
 
@@ -17,7 +15,7 @@ HELP_TXT = """**<u>‼️ HELP MENU ‼️</u>**
 https://t.me/b/botusername/4321
 ```
 
-**⁉️ MULTI POSTS** ♻️
+**⁉️ <u>MULTI POSTS</u>** ♻️
 
 **__Send public/private posts link as Explained above with format "from - to" to send multiple messages like below__**
 

e457211125608fa3a5da8cd1697b8f11c8cde8ac
NeonAnurag <justasneon@gmail.com>
2025-05-15 07:19:01 +0530

    Update strings.py

--- MyselfNeon/strings.py
+++ MyselfNeon/strings.py
@@ -6,25 +6,27 @@ HELP_TXT = """**<u>‼️ HELP MENU ‼️</u>**
 
 **⁉️ FOR PRIVATE CHATS** 👀
 
-**__First send invite link of the chat then send post/s link \n(Unnecessary if the Account of string session is already member of the Chat)__**
+**__First send invite link of the Chat then send post/s link Correctly 🖇️ \n(Unnecessary if the Account of String Session is already member of the Chat)__**
 
 
 **⁉️ FOR BOT CHATS** 🤖
 
-**__Send link with '/b/', Bot's Username and message id, you might want to install some Unofficial client to get the ID like below__**
+**__Send link with '/b/', Bot's Username and message id, you might need to Install some Unofficial client to get the ID like below__**
 
-```https://t.me/b/botusername/4321
+```
+https://t.me/b/botusername/4321
 ```
 
 **⁉️ MULTI POSTS** ♻️
 
-**__Send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
+**__Send public/private posts link as Explained above with format "from - to" to send multiple messages like below__**
 
-```https://t.me/xxxx/1001-1010
+```
+https://t.me/xxxx/1001-1010
 
 https://t.me/c/xxxx/101 - 120
 ```
-**💢 space in between doesn't matter**"""
+**❌ Space in between doesn't matter ❌**"""
        
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
